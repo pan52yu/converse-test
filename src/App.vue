da9ba1cd56aa4ef67b76178a9d1d5dc72484020c
huyu <yhu8836@gmail.com>
2023-11-16 09:36:59 +0800

    add converse.js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <ChatComponent></ChatComponent>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ChatComponent from './components/ChatComponent.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    ChatComponent
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/ChatComponent.vue
@@ -0,0 +1,25 @@
+<script>
+import "/public/dist/converse.js"
+import "/public/dist/converse.css"
+export default {
+  name: "ChatComponent",
+  mounted() {
+    converse.initialize({
+        authentication: 'login', // 身份验证方式 login
+        auto_away: 300, // 5分钟后自动离开
+        auto_reconnect: true, // 自动重连
+        bosh_service_url: 'https://conversejs.org/http-bind/', // Please use this connection manager only for testing purposes
+        message_archiving: 'always', // 消息存档
+        view_mode: 'fullscreen' // 全屏模式
+    });
+  }
+}
+</script>
+
+<template>
+<div id="conversejs-bg"></div>
+</template>
+
+<style scoped>
+
+</style>
