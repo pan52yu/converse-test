ff7607a5c955e5b4db3dbd10a2617e78d49ef77a
huyu <yhu8836@gmail.com>
2023-11-21 14:39:39 +0800

    fix: 却换bosh_service_url

--- src/components/ChatComponent.vue
+++ src/components/ChatComponent.vue
@@ -1,5 +1,5 @@
 <script>
-import "/public/dist/converse.js"
+import "/public/dist/converse.min.js"
 import "/public/dist/converse.css"
 export default {
   name: "ChatComponent",
@@ -8,9 +8,12 @@ export default {
         authentication: 'login', // 身份验证方式 login
         auto_away: 300, // 5分钟后自动离开
         auto_reconnect: true, // 自动重连
-        bosh_service_url: 'https://conversejs.org/http-bind/', // Please use this connection manager only for testing purposes
+        // bosh_service_url: 'https://conversejs.org/http-bind/', // BOSH 服务地址 请仅将此连接管理器用于测试目的
+        bosh_service_url: 'http://herons.top:5280/bosh', // BOSH 服务地址
+        show_controlbox_by_default: true, // 显示控制台
         message_archiving: 'always', // 消息存档
-        view_mode: 'fullscreen' // 全屏模式
+        view_mode: 'fullscreen', // 全屏模式
+        // websocket_url: 'http://herons.top:5280/ws', // websocket 服务地址
     });
   }
 }
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 配置跨域
+  devServer: {
+    proxy: {
+      '/ws': {
+        target: 'http://herons.top:5280', // 代理地址
+        changeOrigin: true, // 是否跨域
+        pathRewrite: {
+          '^/ws': '/ws' // 重写路径
+        },
+      }
+    }
+  }
 })
